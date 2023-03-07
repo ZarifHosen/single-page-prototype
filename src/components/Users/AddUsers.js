69af0e42cf06735fc3a62f71a4e59c4e44e54d50
Zarif Hosen <zarifhosen2015@gmail.com>
2023-03-07 15:23:28 +0600

    bug fixed on const useState

--- src/components/Users/AddUsers.js
+++ src/components/Users/AddUsers.js
@@ -1,25 +1,36 @@
-import React from 'react';
-import classes from './AddUsers.module.css';
-import Card from '../UI/Card';
-import Buttons from '../UI/Buttons';
+import React, { useState } from "react";
+import classes from "./AddUsers.module.css";
+import Card from "../UI/Card";
+import Buttons from "../UI/Buttons";
 
 function AddUsers() {
+    const [enteredUsername, setEnteredUsername] = useState("");
+    const [enteredAge, setEnteredAge] = useState("");
+
     const addUserHandler = (event) => {
         event.preventDefault();
+        console.log(enteredUsername, enteredAge);
+    };
+
+    const usernameChangeHandler = (event) => {
+        setEnteredUsername(event.target.value);
+    };
+
+    const ageChangeHandler = (event) => {
+        setEnteredAge(event.target.value);
     };
 
     return (
         <Card className={classes.input}>
             <form onSubmit={addUserHandler}>
                 <label htmlFor="username">User Name:</label>
-                <input id="username" type="text" />
+                <input id="username" type="text" onChange={usernameChangeHandler} />
                 <label htmlFor="age">Age (Years):</label>
-                <input id="age" type="number" />
+                <input id="age" type="number" onChange={ageChangeHandler} />
                 <Buttons type="submit">Add User</Buttons>
             </form>
         </Card>
     );
 }
-
 
 export default AddUsers;
