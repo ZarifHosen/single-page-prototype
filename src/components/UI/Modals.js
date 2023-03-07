341568e7c3a24bb95b3fbb62b954e1e6628b577f
Zarif Hosen <zarifhosen2015@gmail.com>
2023-03-07 23:20:44 +0600

    modal styled!

--- src/components/UI/Modals.js
+++ src/components/UI/Modals.js
@@ -8,12 +8,21 @@ const Modals = (props) => {
             aria-labelledby="modal-title"
             aria-describedby="modal-description"
         >
-            <Box sx={{ bgcolor: "background.paper", p: 2 }}>
-                <Typography variant="h5" component="h2">
+            <Box sx={{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                bgcolor: "background.paper",
+                boxShadow: 24,
+                p: 4,
+                borderRadius: 2,
+            }}>
+                <Typography variant="h5" component="h2" sx={{ color: "text.primary", mb: 2 }}>
                     Error
                 </Typography>
-                <Typography variant="body1">{props.error}</Typography>
-                <Button onClick={() => props.setError(null)}>Close</Button>
+                <Typography variant="body1" sx={{ color: "text.secondary", mb: 2 }}>{props.error}</Typography>
+                <Button onClick={() => props.setError(null)} sx={{ color: "text.primary" }}>Close</Button>
             </Box>
         </Modal>
 
